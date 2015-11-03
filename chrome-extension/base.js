f97ff83c5944fe41cca6e81607a2fb21885a96dc
Luca Lischetti <sirlisko@gmail.com>
2015-11-03 20:05:07 +0000

    improved the code and the compatibility when not logged

--- chrome-extension/base.js
+++ chrome-extension/base.js
@@ -1,37 +1,22 @@
 (function(){
 	'use strict';
-	var button;
-
-	function handleApp(){
-		button.className = 'sproxify sproxify-app';
-		button.href = 'spotify' + window.location.pathname.replace(/\//g, ':');
-		button.innerHTML = 'Play me in Spotify App!';
-		var login = document.getElementById('login-features');
-		if(login){
-			login.appendChild(button);
-		} else {
-			document.getElementById('notification-area').appendChild(button);
-			setTimeout(function(){
-				button.parentNode.removeChild(button);
-			}, 5000);
-		}
-	}
-
-	function handleWeb(){
-		button.className = 'sproxify sproxify-web';
-		button.href = window.location.href.replace('open', 'play');
-		button.innerHTML = 'Play me in Spotify Web!';
-		document.getElementById('header').firstElementChild.appendChild(button);
-	}
-
-	button = document.createElement('a');
 
 	if(window.location.pathname !== '/'){
+		var button = document.createElement('a');
+		button.className = 'sproxify';
+
 		if(window.location.hostname === 'open.spotify.com'){
-			handleWeb();
+			button.href = window.location.href.replace('open', 'play');
+			button.innerHTML = 'Play me in Spotify Web!';
 		} else {
-			handleApp();
+			button.href = 'spotify' + window.location.pathname.replace(/\//g, ':');
+			button.innerHTML = 'Play me in Spotify App!';
 		}
+
+		document.body.appendChild(button);
+		setTimeout(function(){
+			button.parentNode.removeChild(button);
+		}, 5000);
 	}
 
 })();
